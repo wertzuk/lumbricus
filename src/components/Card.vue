328a8df9d183059b73289457d0962314985c602b
wertzuk <pascalb95@gmx.de>
2022-07-23 19:00:59 +0200

    event cards styling

---
+++ src/components/Card.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="card rounded-1 flex" @click="showContent = !showContent">
+    <div class="card__img">
+      <img src="../assets/event_images/IMG_4767.jpg" class="rounded-1" alt="" />
+    </div>
+    <div class="card__title">
+      Kinder-Ferien-Programm von NABU/NAJU Saarland e.V.: Sommercamp „Fuchsbau“
+    </div>
+    <div class="card__date">SA. 28.05.2022 14:00-16:00 Uhr</div>
+    <div class="card__content">„Ach schüttel mich, ach rüttel mich…“</div>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const showContent = ref(false);
+    return {
+      showContent,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.fade-enter-from {
+  background: red;
+}
+.fade-enter-to {
+  background: limegreen;
+}
+.card {
+  flex-direction: column;
+  background: whitesmoke;
+  padding: 2rem;
+  width: 80%;
+  box-shadow: 3px -3px 15px rgba(0, 0, 0, 0.1);
+  transition: transform 200ms;
+  cursor: pointer;
+  & > * {
+    margin-bottom: 1.5rem;
+    font-size: 19px;
+  }
+  &__img {
+    width: 100%;
+    height: 200px;
+    margin-inline: auto;
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+  &__title {
+  }
+  &__date {
+    font-weight: bolder;
+  }
+
+  &:hover {
+    transform: translateY(-20px);
+  }
+}
+</style>
--- src/views/Vortragsveranstaltungen.vue
+++ src/views/Vortragsveranstaltungen.vue
@@ -1,97 +1,105 @@
 <template>
-  <div class="wrapper">
-    <section class="event-cards"></section>
-    <section class="type">
-      <div class="top flex">
-        <div class="who">
-          <h2>Für wen?</h2>
-          <p>
-            Unsere Vortragsveranstaltungen richten sich vor allem an Obst- und
-            Gartenbauvereine, Landfrauenverbände, aber natürlich auch an andere
-            interessierte Gruppen, Lehrerkollegien, Kindergartenteams und sind
-            auch geeignet für die Weiterbildung in Kommunen und
-            Gartenbaubetrieben.
-          </p>
-        </div>
-        <div class="how">
-          <h2>Wie?</h2>
-          <p>
-            Unsere Vorträge werden mit modernster Technik, praxisnah und vor
-            allem kurzweilig durchgeführt.
-          </p>
-        </div>
+  <div class="wrapper flex">
+    <section class="event-cards">
+      <h2 class="view-heading heading">Vortragsveranstaltungen</h2>
+      <div class="cards flex">
+        <Card class="card" />
+        <Card class="card" />
+        <Card class="card" />
+        <Card class="card" />
+        <Card class="card" />
       </div>
-      <div class="main">
-        <h3>
-          Auf meine lockere, humorvolle Art und Weise werde ich jeden so in das
-          jeweilige Thema einbinden können, dass ihm dieser Abend noch lange in
-          Erinnerung bleibt.
-        </h3>
-        <p>
-          Unsere Vortragsveranstaltungen richten sich vor allem an Obst- und
-          Gartenbauvereine, Landfrauenverbände, aber natürlich auch an andere
-          interessierte Gruppen, Lehrerkollegien, Kindergartenteams und sind
-          auch geeignet für die Weiterbildung in Kommunen und
-          Gartenbaubetrieben.
-        </p>
-
-        <p>
-          Auf meine lockere, humorvolle Art und Weise werde ich jeden so in das
-          jeweilige Thema einbinden können, dass ihm dieser Abend noch lange in
-          Erinnerung bleibt.
-        </p>
-        <p>
-          In fast allen Bereichen des Gartenbaus, und hier vor allem in der
-          biologischen Gartenbewirtschaftung, aber auch in den Themenbereichen –
-          Natur – Umwelt – Gesundheit – könnt ihr lehrreiche, interessante und
-          humorvoll dargebotene Vorträge buchen.
-        </p>
-        <p>
-          Unsere „Pflanzenportraits“ lassen euch die jeweilige Pflanze danach
-          mit anderen Augen sehen (z.B. die Brennnessel). Lasst euch per Telefon
-          oder Mail unser komplettes Angebot an Vortragsveranstaltungen
-          unverbindlich mitteilen.
-        </p>
-        <p>
-          Theoretisch und Praxisorientiert finden unsere Schnittkurse für
-          Obstgehölze Rosen Sträucher statt. Geeignete Stellen für die
-          praktische Durchführung sollten vor Ort zur Verfügung stehen.
-        </p>
-        <p>
-          Vorträge können auch im Rosengarten des Umwelt- und Freizeitzentrums
-          Finkenrech (bei Dirmingen) gehalten werden.
-        </p>
-      </div>
+    </section>
+    <section class="event-description">
+      <p>
+        Unsere Vortragsveranstaltungen richten sich vor allem an Obst- und
+        Gartenbauvereine, Landfrauenverbände, aber natürlich auch an andere
+        interessierte Gruppen, Lehrerkollegien, Kindergartenteams und sind auch
+        geeignet für die Weiterbildung in Kommunen und Gartenbaubetrieben.
+      </p>
+      <p>
+        Unsere Vorträge werden mit modernster Technik, praxisnah und vor allem
+        kurzweilig durchgeführt.
+      </p>
+      <p>
+        Auf meine lockere, humorvolle Art und Weise werde ich jeden so in das
+        jeweilige Thema einbinden können, dass ihm dieser Abend noch lange in
+        Erinnerung bleibt.
+      </p>
+      <p>
+        In fast allen Bereichen des Gartenbaus, und hier vor allem in der
+        biologischen Gartenbewirtschaftung, aber auch in den Themenbereichen –
+        Natur – Umwelt – Gesundheit – könnt ihr lehrreiche, interessante und
+        humorvoll dargebotene Vorträge buchen.
+      </p>
+      <p>
+        Unsere „Pflanzenportraits“ lassen euch die jeweilige Pflanze danach mit
+        anderen Augen sehen (z.B. die Brennnessel). Lasst euch per Telefon oder
+        Mail unser komplettes Angebot an Vortragsveranstaltungen unverbindlich
+        mitteilen.
+      </p>
+      <p>
+        Theoretisch und Praxisorientiert finden unsere Schnittkurse für
+        Obstgehölze Rosen Sträucher statt. Geeignete Stellen für die praktische
+        Durchführung sollten vor Ort zur Verfügung stehen.
+      </p>
+      <p>
+        Vorträge können auch im Rosengarten des Umwelt- und Freizeitzentrums
+        Finkenrech (bei Dirmingen) gehalten werden.
+      </p>
     </section>
   </div>
 </template>
 
 <script>
-export default {};
+import Card from '@/components/Card.vue';
+
+export default {
+  components: {
+    Card,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 @import '../scss/vars';
 .wrapper {
-  display: grid;
-  grid-template-columns: 1fr 2fr;
-  & div {
-    border-bottom: 1px solid black;
-    border-right: 1px solid black;
+  & > * {
+    padding: 3em;
+    color: $clr-text-400;
   }
-  //   grid-
-}
 
-.type {
-  p {
-    margin-bottom: 2em;
+  .cards {
+    margin-top: 3rem;
+    flex-direction: column;
+    .card {
+      z-index: 1;
+    }
+    .card:nth-child(2n) {
+      background: $clr-card-bg;
+    }
+    .card:not(:first-child) {
+      margin-top: calc(6rem * -1);
+    }
   }
-}
 
-.how,
-.who,
-.main {
-  padding: 2em 3em;
-  font-size: $fs-400;
+  .view-heading {
+    font-size: $fs-900;
+  }
+  .event-cards {
+    flex-basis: 20%;
+    flex-shrink: 1;
+  }
+  .event-description {
+    flex-basis: 70%;
+    margin-top: 4em;
+
+    p {
+      font-size: $fs-400;
+      margin-bottom: 1.5em;
+      width: 80%;
+      margin-inline: auto;
+    }
+  }
 }
 </style>
