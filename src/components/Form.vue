e28fbfd617b1624673ababed968d913c7d308cd7
wertzuk <pascalb95@gmx.de>
2023-04-23 11:34:23 +0200

    mark requried fields

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,11 +1,11 @@
 <template>
   <form>
     <div class="flex row">
-      <div class="flex flex-col">
+      <div class="flex flex-col required">
         <label for="vorname">Vorname</label>
         <input type="text" id="vorname" required />
       </div>
-      <div class="flex flex-col">
+      <div class="flex flex-col required">
         <label for="nachname">Nachname</label>
         <input type="text" id="nachname" required />
       </div>
@@ -21,14 +21,18 @@
       </div>
     </div>
     <div class="flex row">
-      <div class="flex flex-col">
+      <div class="flex flex-col required">
         <label for="email">E-Mail</label>
         <input type="email" id="email" required />
       </div>
-      <div class="flex flex-col">
-        <label for="comment">Kommentar (Optional)</label>
-        <textarea id="comment" name="w3review" rows="4" cols="15"></textarea>
+      <div class="flex flex-col required">
+        <label for="tel">Telefon</label>
+        <input type="tel" id="tel" required />
       </div>
+    </div>
+    <div class="flex flex-col">
+      <label for="comment">Kommentar (Optional)</label>
+      <textarea id="comment" name="w3review" rows="4" cols="15"></textarea>
     </div>
     <div class="flex justify-center">
       <button type="submit" class="submit">Anmelden</button>
@@ -71,13 +75,17 @@ textarea {
   }
 }
 
+.required label::after {
+  content: ' *';
+  color: red;
+}
+
 .submit {
   margin-top: 1.5rem;
   padding: 0.7rem 2rem;
   background: $clr-beige;
   border: 1px solid black;
   outline: none;
-  // box-shadow: 0 0 8px 2px  rgba(0, 0, 0, 0.2);
   &:hover {
     opacity: 0.7;
   }
